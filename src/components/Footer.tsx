a86bf5dab2e772a56449ff43ba936d25192101dc
tripathiabhishek07 <abhishektripathi3276@gmail.com>
2024-12-08 01:47:45 +0530

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <div className="bg-orange-500 py-10">
+    <div className="bg-orange-500 py-5">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <span className="text-3xl text-white font-bold tracking-tight">
-          MernEats.com
+        <span className="text-2xl text-white font-bold tracking-tight ">
+          Krispy & Krunchy Bites
         </span>
         <span className="text-white font-bold tracking-tight flex gap-4">
           <span>Privacy Policy</span>
